5680006b5d3c57053cf3f0f6a4d692d42b0f4a4c
baer1 <videocreator@outlook.de>
2024-12-07 00:34:31 +0100

    Add legacy items

---
+++ Items/legacy.cs
@@ -0,0 +1,37 @@
+namespace whereThat1percentAt.Items
+{
+    class crimsonCompass : CrimsonCompass
+    {
+        public override void AddRecipes() { }
+    }
+
+    class corruptionCompass : CorruptionCompass
+    {
+        public override void AddRecipes() { }
+    }
+
+    class hallowCompass : HallowCompass
+    {
+        public override void AddRecipes() { }
+    }
+
+    class crimsonMirror : CrimsonMirror
+    {
+        public override void AddRecipes() { }
+    }
+
+    class corruptionMirror : CorruptionMirror
+    {
+        public override void AddRecipes() { }
+    }
+
+    class hallowMirror : HallowMirror
+    {
+        public override void AddRecipes() { }
+    }
+
+    class rainbowCompass : RainbowCompass
+    {
+        public override void AddRecipes() { }
+    }
+}
