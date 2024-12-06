5e57d87b33b221738d4be22a2c90a396f748ddeb
baer1 <videocreator@outlook.de>
2024-12-06 23:00:44 +0100

    Move compass logic to abstract class

---
+++ Items/baseModCompass.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace whereThat1percentAt.Items
+{
+    public abstract class baseModCompass : ModItem
+    {
+        public abstract short CompassItem { get; }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.Compass);
+            Item.value = Item.value + new Item(CompassItem).value * 5 + 1;
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Compass);
+            recipe.AddIngredient(CompassItem, 5);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
--- Items/crimsonCompass.cs
+++ Items/crimsonCompass.cs
@@ -1,29 +1,13 @@
 using Terraria;
 using Terraria.ID;
-using Terraria.ModLoader;
 using whereThat1percentAt.Content;
 
 namespace whereThat1percentAt.Items
 {
-    public class crimsonCompass : ModItem
+    public class crimsonCompass : baseModCompass
     {
         public override string Texture => Textures.crimsonCompass;
-
-        public override void SetDefaults()
-        {
-            Item.CloneDefaults(ItemID.Compass);
-            Item.value = Item.value + new Item(ItemID.Vertebrae).value * 5 + 1;
-            Item.rare = ItemRarityID.Green;
-        }
-
-        public override void AddRecipes()
-        {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ItemID.Compass);
-            recipe.AddIngredient(ItemID.Vertebrae, 5);
-            recipe.AddTile(TileID.WorkBenches);
-            recipe.Register();
-        }
+        public override short CompassItem => ItemID.Vertebrae;
 
         public override void UpdateInfoAccessory(Player player)
         {
